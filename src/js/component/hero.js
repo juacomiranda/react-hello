42094c4a862f18315c21a9e56fa718fccc077563
juacomiranda <jmiranda@live.cl>
2022-08-07 16:39:34 +0000

    cambios en el hero

---
+++ src/js/component/hero.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Hero(){
+    return (
+        <div className = "container">
+            <h1>Pizzas 4Geeks</h1> 
+            <p>Aprende hacer pizzas con 4Geeks Academy Chile</p>             
+            <button type="button" className="btn btn-primary">Conocenos</button>
+        </div>
+
+    );
+}
+
+export default Hero;
+
---
+++ src/js/component/nav.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+function Navbar(){
+    return ( 
+        <nav className="navbar navbar-expand-lg bg-danger">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">Pizzas</a>
+          <div className="collapse navbar-collapse">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link" href="#">Home</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">About</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Services</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Contact</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+     );
+ }
+ 
+ export default Navbar;
